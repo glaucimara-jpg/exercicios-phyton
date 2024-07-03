5e57d9c73cdb05f1e1a5bde1fb63e3153cc8b9bf
Professora Glau <80214705+glaucimara-jpg@users.noreply.github.com>
2024-07-03 18:51:58 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -1,3 +1,4 @@
+#exercicio 1 INT
 num1 = 12
 
 num2 = int(15)
@@ -10,3 +11,66 @@ print(type(num1))
 
 print(num2)
 print(type(num2))
+
+
+
+#exercicio 2 INT
+
+num1 = 12
+# Será automaticamente identificado pelo interpretador como dado do tipo int
+
+num2 = int(15)
+# Será manualmente identificado pelo interpretador como dado do tipo int
+
+num3 = str(15)
+# Será manualmente identificado pelo interpretador como dado do tipo string
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+
+#exercicio 3 FLOAT
+
+num3 = 12.8
+# Ao inserir o ponto demarcador de casa decimal, tal número será
+# automaticamente reconhecido pelo interpretador como tipo float
+
+num4 = float(19.0)
+
+print(num3)
+print(type(num3))
+
+print(num4)
+print(type(num4))
+
+
+#exercicio 4 String
+
+palavra1 = 'palavra'
+# Todo conteúdo contido entre aspas é lido como texto pelo interpretador
+
+palavra2 = "marca d'água"
+# Uso de aspas duplas para não gerar conflito com o apóstrofo '
+
+frase1 = 'Uma frase qualquer'
+
+frase3 = 'Ela tinha 8 anos'
+
+frase3 = str('Outra frase qualquer')
+
+print(palavra1)
+print(palavra2)
+
+print(frase3)
+
+#exercicio 5 Bool
+
+condicao1 = True
+# Tipo de dado usado em expressões/proposições lógicas, onde o retorno é True
+# ou False, referente a Verdadeiro ou Falso, válido ou inválido, ligado ou
+# desligado, etc... de acordo com a proposição.
+
+print(condicao1)
+print(type(condicao1)
+print(type(frase3))
