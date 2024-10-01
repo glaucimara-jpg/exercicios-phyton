bb13fbd4d9463717b2376ed7d0173c4fe774bb92
Professora Glau <80214705+glaucimara-jpg@users.noreply.github.com>
2024-10-01 14:23:04 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -1,76 +1,25 @@
-#exercicio 1 INT
-num1 = 12
 
-num2 = int(15)
+#funções:
 
-print(num1)
-# Exibe em tela o conteúdo da variável num1
-
-print(type(num1))
-# Exibe em tela o tipo de dado da variável num1
-
-print(num2)
-print(type(num2))
+def numero_par_ou_impar(numero):
+    return numero % 2 == 0
 
 
+numero = int(input('Informe um número inteiro qualquer: '))
 
-#exercicio 2 INT
-
-num1 = 12
-# Será automaticamente identificado pelo interpretador como dado do tipo int
-
-num2 = int(15)
-# Será manualmente identificado pelo interpretador como dado do tipo int
-
-num3 = str(15)
-# Será manualmente identificado pelo interpretador como dado do tipo string
-
-print(type(num1))
-print(type(num2))
-print(type(num3))
+if numero_par_ou_impar(numero):
+    print(f'O número {numero} é par')
+else:
+    print(f'O número {numero} é impar')
 
 
-#exercicio 3 FLOAT
+#mensagens:
+nome = input('Qual é o seu nome? ')
+print(f'Olá {nome}, muito prazer.')
 
-num3 = 12.8
-# Ao inserir o ponto demarcador de casa decimal, tal número será
-# automaticamente reconhecido pelo interpretador como tipo float
+#datas:
 
-num4 = float(19.0)
+from datetime import datetime
 
-print(num3)
-print(type(num3))
-
-print(num4)
-print(type(num4))
-
-
-#exercicio 4 String
-
-palavra1 = 'palavra'
-# Todo conteúdo contido entre aspas é lido como texto pelo interpretador
-
-palavra2 = "marca d'água"
-# Uso de aspas duplas para não gerar conflito com o apóstrofo '
-
-frase1 = 'Uma frase qualquer'
-
-frase3 = 'Ela tinha 8 anos'
-
-frase3 = str('Outra frase qualquer')
-
-print(palavra1)
-print(palavra2)
-
-print(frase3)
-
-#exercicio 5 Bool
-
-condicao1 = True
-# Tipo de dado usado em expressões/proposições lógicas, onde o retorno é True
-# ou False, referente a Verdadeiro ou Falso, válido ou inválido, ligado ou
-# desligado, etc... de acordo com a proposição.
-
-print(condicao1)
-print(type(condicao1)
-print(type(frase3))
+data_atual = datetime.now()
+print(data_atual.date())
